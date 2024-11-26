79b18b6a49f5dec91cf2e10ac799ba7db77debcc
Mubashar <mubashair@gmail.com>
2024-11-26 19:47:15 +0500

    entity created

---
+++ src/main/java/com/prog/Hotel_Management/Entity/Hotel.java
@@ -0,0 +1,62 @@
+package com.prog.Hotel_Management.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Hotel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String city;
+	private String address;
+	private float rating;
+	@Column(name = "available")
+	private boolean isAvailable;
+	
+	
+	//Getter and Setter
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public float getRating() {
+		return rating;
+	}
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	
+	
+	
+}
